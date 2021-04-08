098f66dfc64e0a998e8812c14810c2a4af786366
ravilushqa <18125561+ravilushqa@users.noreply.github.com>
2021-04-08 19:29:13 +0000

    [LeetCode Sync] Runtime - 4 ms (1.90%), Memory - 4.4 MB (100.00%)

---
+++ submissions/0104-maximum-depth-of-binary-tree/solution.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
+
+func max(int1, int2 int) int {
+	if int1 > int2 {
+		return int1
+	}
+
+	return int2
+}
