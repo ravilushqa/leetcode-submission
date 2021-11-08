9d40a56c658bc29329f081dc5aac0d029604fbfb
ravilushqa <18125561+ravilushqa@users.noreply.github.com>
2021-11-08 07:52:43 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.6 MB (100.00%)

--- submissions/0206-reverse-linked-list/solution.go
+++ submissions/0206-reverse-linked-list/solution.go
@@ -7,14 +7,10 @@
  */
 func reverseList(head *ListNode) *ListNode {
     var prev *ListNode
-    for head != nil {
-        next := head.Next
-        head.Next = prev
-        
-        prev = head
-        
-        head = next
-    }
+
+	for head != nil {
+		head.Next, prev, head = prev, head, head.Next
+	}
     
     return prev
 }
