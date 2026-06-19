5d2bc97a1ebf59679690096da712b890c77cc9b3
ravilushqa <18125561+ravilushqa@users.noreply.github.com>
2026-06-19 12:19:16 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6 MB (31.51%)

--- submissions/0001-two-sum/solution.go
+++ submissions/0001-two-sum/solution.go
@@ -1,12 +1,12 @@
 func twoSum(nums []int, target int) []int {
-    lookup := make(map[int]int)
-
-    for i, v := range nums {
-        j, ok := lookup[-v]
-        lookup[v - target] = i
-        if ok {
-            return []int{j, i}
+    m := map[int]int{}
+    for i,v := range nums {
+        if j, exist := m[target-v]; exist {
+            return []int{j,i}
         }
+
+        m[v] = i
     }
-    return []int{}
+
+    return nil
 }
