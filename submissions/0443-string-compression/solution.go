a02ae14531442f65d20299a2226ff21633ef3192
ravilushqa <18125561+ravilushqa@users.noreply.github.com>
2026-08-02 12:08:17 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 4.7 MB (99.49%)

---
+++ submissions/0443-string-compression/solution.go
@@ -0,0 +1,33 @@
+import "strconv"
+
+func compress(chars []byte) int {
+    readIdx, writeIdx := 0, 0
+
+    for readIdx < len(chars) {
+        ch := chars[readIdx]
+        count := 0
+
+        for readIdx < len(chars) && chars[readIdx] == ch {
+            count++
+            readIdx++
+        }
+
+        if count == 1 {
+            chars[writeIdx] = ch
+            writeIdx++
+
+            continue
+        }
+
+        chars[writeIdx] = ch
+        writeIdx++
+
+        for _, digit := range []byte(strconv.Itoa(count)) {
+            chars[writeIdx] = digit
+            writeIdx++
+        }
+    }
+
+    return writeIdx
+}
+
