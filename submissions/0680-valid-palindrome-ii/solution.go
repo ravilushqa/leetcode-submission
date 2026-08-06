549ec642f87642a31a18aa3155655c5a7dc445be
ravilushqa <18125561+ravilushqa@users.noreply.github.com>
2026-08-06 10:09:38 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.7 MB (65.92%)

---
+++ submissions/0680-valid-palindrome-ii/solution.go
@@ -0,0 +1,25 @@
+func validPalindrome(s string) bool {
+	i, j := 0, len(s)-1
+
+	for i < j {
+		if s[i] != s[j] {
+			return checkPalindrome(s, i+1, j) || checkPalindrome(s, i, j-1)
+		}
+		i++
+		j--
+	}
+
+	return true
+}
+
+func checkPalindrome(s string, i, j int) bool {
+	for i < j {
+		if s[i] != s[j] {
+			return false
+		}
+		i++
+		j--
+	}
+
+	return true
+}
