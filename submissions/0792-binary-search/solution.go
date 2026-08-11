ba9827736f1a855e9741ba52ab5a29343edb087f
ravilushqa <18125561+ravilushqa@users.noreply.github.com>
2026-08-11 15:37:13 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.5 MB (78.48%)

--- submissions/0792-binary-search/solution.go
+++ submissions/0792-binary-search/solution.go
@@ -1,18 +1,20 @@
 func search(nums []int, target int) int {
     l := 0
     r := len(nums) - 1
+    
 
-    for l <=r {
-        m := (l + r) / 2
+    for l<=r {
+        m := (l+r)/2
 
-        if target < nums[m] {
-            r = m - 1
-        } else if target > nums[m] {
+        if target > nums[m] {
             l = m + 1
+        } else if target < nums[m] {
+            r = m - 1
         } else {
             return m
         }
     }
 
     return -1
+    
 }
