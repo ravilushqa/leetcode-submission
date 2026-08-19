dfd9be311c75c07185660daa9706466f43b196b6
ravilushqa <18125561+ravilushqa@users.noreply.github.com>
2026-08-19 10:50:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 4.4 MB (71.86%)

--- submissions/0021-merge-two-sorted-lists/solution.go
+++ submissions/0021-merge-two-sorted-lists/solution.go
@@ -5,21 +5,29 @@
  *     Next *ListNode
  * }
  */
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+    prevHead := &ListNode{}
+    cur := prevHead
+    for list1 != nil || list2 != nil {
+        if list1 == nil {
+            cur.Next = list2
+            break
+        }
+        if list2 == nil {
+            cur.Next = list1
+            break
+        }
+        
+        if list1.Val < list2.Val {
+            cur.Next = list1
+            list1 = list1.Next
+        } else {
+            cur.Next = list2
+            list2 = list2.Next
+        }
 
-func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode { 
-    if list1 == nil {
-        return list2
-    }
-    
-    if list2 == nil {
-        return list1
+        cur = cur.Next
     }
 
-    if list1.Val < list2.Val {
-        list1.Next = mergeTwoLists(list1.Next, list2)
-        return list1
-    }
-
-    list2.Next = mergeTwoLists(list2.Next, list1)
-    return list2
+    return prevHead.Next
 }
