370d6c11d57ba0c5bf09508eb22f5961a326ca89
ravilushqa <18125561+ravilushqa@users.noreply.github.com>
2026-08-19 09:50:27 +0000

    [LeetCode Sync] Runtime - 34 ms (50.00%), Memory - 8.7 MB (50.00%)

--- submissions/2890-count-substrings-without-repeating-character/solution.go
+++ submissions/2890-count-substrings-without-repeating-character/solution.go
@@ -1,24 +1,19 @@
-// a-z
-// bruteforce: n^2
-// s: abababa 3+2+1 = 6
-// abab
 func numberOfSpecialSubstrings(s string) int {
-    res := 0
     start := 0
+    res := 0
 
     var chars [26]int
 
-    for end, char := range s {
-        chars[char-'a']+=1
-
-        for chars[char-'a'] > 1 {
-            chars[s[start]-'a']--
+    for end, v := range s {
+        chars[v - 'a']++
+        
+        for chars[v - 'a'] > 1 {
+            chars[s[start] - 'a']--
             start++
         }
-        
+
         res += end - start + 1
-    } 
+    }
 
     return res
-
 }
