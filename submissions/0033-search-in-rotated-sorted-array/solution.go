cefbf07f3eeebdd00c28524fa3cf67c5e9d10884
ravilushqa <18125561+ravilushqa@users.noreply.github.com>
2026-08-25 10:05:37 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 4.4 MB (72.52%)

--- submissions/0033-search-in-rotated-sorted-array/solution.go
+++ submissions/0033-search-in-rotated-sorted-array/solution.go
@@ -1,31 +1,25 @@
-// [4,5,6,7,0,1,2]
-
 func search(nums []int, target int) int {
-    l := 0
-    r := len(nums) - 1
+    l, r := 0, len(nums) - 1
 
     for l <= r {
         m := (l + r) / 2
+
         if nums[m] == target {
             return m
         }
 
-        // left side
-        if nums[m] >= nums[l] {
-            if target > nums[m] {
-                l = m + 1
-            } else if target < nums[l] {
-                l = m + 1
-            } else {
+        //[l..m]
+        if nums[l] <= nums[m] {
+            if nums[l] <= target && target <= nums[m] {
                 r = m - 1
+            } else {
+                l = m + 1
             }
-        } else { // right side
-            if target < nums[m] {
-                r = m - 1
-            } else if target > nums[r] {
-                r = m - 1
-            } else {
+        } else { //[m..r]
+            if nums[m] <= target && target <= nums[r] {
                 l = m + 1
+            } else {
+                r = m - 1
             }
         }
     }
