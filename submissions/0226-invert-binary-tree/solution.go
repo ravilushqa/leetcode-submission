21f336ee16dda11622a22cf0eae19577b1e5a262
ravilushqa <18125561+ravilushqa@users.noreply.github.com>
2026-08-25 14:21:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 4.1 MB (36.06%)

--- submissions/0226-invert-binary-tree/solution.go
+++ submissions/0226-invert-binary-tree/solution.go
@@ -7,12 +7,11 @@
  * }
  */
 func invertTree(root *TreeNode) *TreeNode {
-    if root == nil || (root.Left == root.Right) {
-        return root
+    if root == nil {
+        return nil
     }
-    
+
     root.Left, root.Right = invertTree(root.Right), invertTree(root.Left)
-    
+
     return root
-    
 }
