3bb5080b22a7848c928e15cea714a1a14db60728
ravilushqa <18125561+ravilushqa@users.noreply.github.com>
2026-08-25 12:49:51 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.2 MB (80.15%)

--- submissions/0209-minimum-size-subarray-sum/solution.go
+++ submissions/0209-minimum-size-subarray-sum/solution.go
@@ -1,21 +1,21 @@
 func minSubArrayLen(target int, nums []int) int {
     res := 0
     start := 0
-
+    
     sum := 0
     for end, v := range nums {
         sum += v
-        
-        for target <= sum {
+
+        for sum >= target {
             if res == 0 {
                 res = end - start + 1
             } else {
                 res = min(res, end - start + 1)
             }
+
             sum -= nums[start]
             start++
-        }
-        
+        } 
     }
 
     return res
