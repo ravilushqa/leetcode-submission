db9217886014455b51ef48eb012f65609f8a0fe1
ravilushqa <18125561+ravilushqa@users.noreply.github.com>
2026-08-26 10:18:58 +0000

    [LeetCode Sync] Runtime - 16 ms (48.23%), Memory - 5.5 MB (16.51%)

--- submissions/0424-longest-repeating-character-replacement/solution.go
+++ submissions/0424-longest-repeating-character-replacement/solution.go
@@ -1,18 +1,19 @@
 func characterReplacement(s string, k int) int {
     res := 0
     start := 0
-
-    var freqs [26]int
+    freqs := [26]int{}
 
     for end, v := range []byte(s) {
         freqs[v-'A']++
-        maxFreq := GetMaxFreq(freqs)
+
         windowLen := end - start + 1
+        maxFreq := topFreq(freqs)
+
         for windowLen - maxFreq > k {
-            freqs[s[start] - 'A']--
+            freqs[s[start]-'A']--
             start++
             windowLen--
-            maxFreq = GetMaxFreq(freqs)
+            maxFreq = topFreq(freqs)
         }
 
         res = max(res, windowLen)
@@ -21,9 +22,9 @@ func characterReplacement(s string, k int) int {
     return res
 }
 
-func GetMaxFreq(freqs [26]int) int {
-    var res int
-    for _,v := range freqs {
+func topFreq(freqs [26]int) int {
+    res := 0
+    for _, v := range freqs {
         res = max(res, v)
     }
 
