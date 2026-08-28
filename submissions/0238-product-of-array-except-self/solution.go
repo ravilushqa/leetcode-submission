c8f45903c4f46cf1d2a5325ed8a9a2a471305feb
ravilushqa <18125561+ravilushqa@users.noreply.github.com>
2026-08-28 11:20:31 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.1 MB (64.28%)

--- submissions/0238-product-of-array-except-self/solution.go
+++ submissions/0238-product-of-array-except-self/solution.go
@@ -1,17 +1,18 @@
 func productExceptSelf(nums []int) []int {
     res := make([]int, len(nums))
 
-    prefix := 1
-    for i, v := range nums {
-        res[i] = prefix
-        prefix *=v
+    // prefix
+    product := 1
+    for i := 0; i < len(nums); i++ {
+        res[i] = product
+        product *= nums[i]
     }
 
-    postfix := 1
+    // suffix
+    product = 1
     for i := len(nums) - 1; i >= 0; i-- {
-        res[i] *= postfix
-        postfix *= nums[i]
+        res[i] *= product
+        product *= nums[i]
     }
-
     return res
 }
