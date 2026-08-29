cbf3b012bc186a165220cbb3e1821305f50b53e7
ravilushqa <18125561+ravilushqa@users.noreply.github.com>
2026-08-29 09:07:30 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.2 MB (26.62%)

--- submissions/1019-squares-of-a-sorted-array/solution.go
+++ submissions/1019-squares-of-a-sorted-array/solution.go
@@ -1,11 +1,18 @@
 func sortedSquares(nums []int) []int {
-    for i, v := range nums {
-        nums[i] *= v
+    res := make([]int, len(nums))
+    start, end := 0, len(nums) - 1
+
+    for start <= end {
+        startSqr := nums[start] * nums[start]
+        endSqr := nums[end] * nums[end]
+        if startSqr > endSqr {
+            res[end-start] = startSqr
+            start++
+        } else {
+            res[end-start] = endSqr
+            end--
+        }
     }
 
-    sort.Slice(nums, func(i,j int) bool {
-        return nums[i] < nums[j]
-    })
-
-    return nums
+    return res
 }
