f1e0999b5125bad7695ffbc338d64f7e7abc4c7f
ravilushqa <18125561+ravilushqa@users.noreply.github.com>
2026-09-01 10:02:07 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 4.9 MB (26.84%)

--- submissions/0443-string-compression/solution.go
+++ submissions/0443-string-compression/solution.go
@@ -1,29 +1,24 @@
 func compress(chars []byte) int {
-    readIdx, writeIdx := 0,  0
-
+    readIdx, writeIdx := 0, 0
+    
     for readIdx < len(chars) {
-        ch := chars[readIdx]
-        count := 0
-
-        for readIdx < len(chars) && chars[readIdx] == ch {
+        char := chars[readIdx]
+        count := 1
+        readIdx++
+        
+        for readIdx < len(chars) && chars[readIdx] == char {
             count++
             readIdx++
         }
 
-        if count == 1 {
-            chars[writeIdx] = ch
-            writeIdx++
-
-            continue
-        } 
-
-        // more then one
-        chars[writeIdx] = ch
+        chars[writeIdx] = char
         writeIdx++
-
-        for _, digit := range []byte(strconv.Itoa(count)) {
-            chars[writeIdx] = digit
-            writeIdx++
+        if count > 1 {
+            digits := strconv.Itoa(count)
+            for _, c := range []byte(digits) {
+                chars[writeIdx] = c
+                writeIdx++
+            }
         }
     }
 
