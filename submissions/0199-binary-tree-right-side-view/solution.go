392093d74517adca5d4a02dce2a15021a4c5d079
ravilushqa <18125561+ravilushqa@users.noreply.github.com>
2026-09-05 08:24:38 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 4.4 MB (22.99%)

--- submissions/0199-binary-tree-right-side-view/solution.go
+++ submissions/0199-binary-tree-right-side-view/solution.go
@@ -7,32 +7,30 @@
  * }
  */
 func rightSideView(root *TreeNode) []int {
+    res := []int{}
     if root == nil {
-        return []int{}
+        return res
     }
-	list := []int{}
-	var queue []*TreeNode
+    queue := []*TreeNode{root}
 
-	queue = append(queue, root)
-
-	for len(queue) > 0 {
-		n := len(queue)
-		for i := 1; i <= n; i++ {
+    for len(queue) > 0 {
+        n := len(queue)
+        for i := 1; i <= n; i++ {
             node := queue[0]
             queue = queue[1:]
-
             if i == n {
-                list = append(list, node.Val)
+                res = append(res, node.Val)
             }
+
             if node.Left != nil {
                 queue = append(queue, node.Left)
             }
+            
             if node.Right != nil {
                 queue = append(queue, node.Right)
             }
-		}
-	}
+        }
+    }
 
-    return list
+    return res
 }
-
