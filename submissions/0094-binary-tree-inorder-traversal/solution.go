7f4eeb639bb01d0aa6f3de7330f5376d4a213141
ravilushqa <18125561+ravilushqa@users.noreply.github.com>
2026-09-21 13:17:05 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 4.1 MB (18.61%)

--- submissions/0094-binary-tree-inorder-traversal/solution.go
+++ submissions/0094-binary-tree-inorder-traversal/solution.go
@@ -7,19 +7,17 @@
  * }
  */
 func inorderTraversal(root *TreeNode) []int {
-    var dfs func(node *TreeNode, res []int) []int
+    return dfs(root, nil)
+}
 
-    dfs = func(node *TreeNode, res []int) []int{
-        if node == nil {
-            return res
-        }
-
-        res = dfs(node.Left, res)
-        res = append(res, node.Val)
-        res = dfs(node.Right, res)
-
+func dfs(node *TreeNode, res []int) []int {
+    if node == nil {
         return res
     }
 
-    return dfs(root, nil)
+    res = dfs(node.Left, res)
+    res = append(res, node.Val)
+    res = dfs(node.Right, res)
+
+    return res
 }
